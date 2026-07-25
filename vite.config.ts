95d11866274ebed55f58e5ac0f6d7281bd0bdeb2
Cheska Huang <julyannahuang@gmail.com>
2026-07-25 23:28:35 +0800

    fix for react-draggable

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
-  resolve: {
-    tsconfigPaths: true,
-  },
+export default defineConfig(({ mode }) => {
+  // we are not using any env variables so this is fine
+  // this is because the react-draggable library uses process.env...
+  // Albeit questionable, will need to raise an issue with the maintainers
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    envDir: false,
+    define: {
+      "process.env": env,
+    },
+    plugins: [tailwindcss(), reactRouter()],
+    resolve: {
+      tsconfigPaths: true,
+    },
+  };
 });
