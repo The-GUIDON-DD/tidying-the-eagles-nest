07e9e06d3738b863825832d61a67e822db643d72
Cheska Huang <julyannahuang@gmail.com>
2026-07-25 23:31:38 +0800

    workaround for react-draggable

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => {
   // we are not using any env variables so this is fine
   // this is because the react-draggable library uses process.env...
   // Albeit questionable, will need to raise an issue with the maintainers
-  const env = loadEnv(mode, process.cwd(), "");
+  // For reference: https://github.com/react-grid-layout/react-draggable/issues/806
   return {
     envDir: false,
     define: {
-      "process.env": env,
+      "process.env.DRAGGABLE_DEBUG": JSON.stringify(
+        process.env.DRAGGABLE_DEBUG ?? false,
+      ),
     },
     plugins: [tailwindcss(), reactRouter()],
     resolve: {
