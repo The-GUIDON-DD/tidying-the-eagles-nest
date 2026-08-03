edf774d4070126773a5852bcc93d1313bf09b2f4
Cheska Huang <julyannahuang@gmail.com>
2026-08-03 16:43:15 +0800

    FINALLY OMG

---
+++ app/components/WinScreen.tsx
@@ -0,0 +1,258 @@
+import {
+  animate,
+  type createLayout,
+  createTimeline,
+  cubicBezier,
+  type Scope,
+  spring,
+} from "animejs";
+import { useEffect, useRef, useState } from "react";
+import { Link } from "react-router";
+
+const MAX_SCREEN_SIZE =
+  "w-screen h-screen max-w-screen min-w-screen max-h-screen min-h-screen";
+
+function BgBanner() {
+  const GRID_OVERLAY_CHILD = "row-start-1 row-span-1 col-start-1 col-span-1";
+
+  useEffect(() => {
+    const BEZIER_OUT = cubicBezier(0.1, 0.7, 0.5, 1);
+    const tl = createTimeline();
+    tl.label("start")
+      .add("#win-white-banner", {
+        x: ["100vw", 0],
+        duration: 300,
+        ease: BEZIER_OUT,
+      })
+      .add("#win-purple-banner", {
+        x: ["-100vw", 0],
+        duration: 300,
+        ease: BEZIER_OUT,
+      })
+      .label("line-entrance")
+      .add(
+        "#win-top-line",
+        {
+          x: ["200vw", "100vw"],
+          duration: 2000,
+          ease: BEZIER_OUT,
+        },
+        "line-entrance",
+      )
+      .add(
+        "#win-bottom-line",
+        {
+          x: ["-200vw", "-100vw"],
+          duration: 2000,
+          ease: BEZIER_OUT,
+        },
+        "line-entrance",
+      )
+      .label("line-scroll")
+      .add(
+        "#win-top-line",
+        {
+          x: ["100vw", 0],
+          duration: 200000,
+          loop: true,
+        },
+        "line-scroll",
+      )
+      .add(
+        "#win-bottom-line",
+        {
+          x: ["-100vw", 0],
+          duration: 200000,
+          loop: true,
+        },
+        "line-scroll",
+      );
+    tl.init();
+  }, []);
+
+  return (
+    <div
+      id="win-screen"
+      className={`fixed inset-0 ${MAX_SCREEN_SIZE} z-50 grid grid-cols-1 grid-rows-1 place-items-center`}
+      style={{
+        background:
+          "radial-gradient(rgba(255,255,255,0.3), rgba(255,255,255,0)), rgba(103,74, 179, 0.6)",
+      }}
+    >
+      <div
+        id="win-white-banner"
+        className={`${GRID_OVERLAY_CHILD} h-[40%] w-full bg-white drop-shadow-[0_4px_21px_37px_rgba(0,0,0,0.1)]`}
+      />
+      <div
+        id="win-purple-banner"
+        className={`${GRID_OVERLAY_CHILD} h-[34%] w-full`}
+        style={{
+          background: "radial-gradient(#5d3e9e, #390f8f), purple",
+          filter: "brightness(0.9)",
+        }}
+      />
+      <div
+        id="win-top-line"
+        className={`${GRID_OVERLAY_CHILD} h-[30%] w-[300vw]`}
+        style={{
+          background:
+            "repeat-x top center / 100vw auto url('/level_win/top-line.svg')",
+        }}
+      />
+      <div
+        id="win-bottom-line"
+        className={`${GRID_OVERLAY_CHILD} h-[30%] w-[300vw]`}
+        style={{
+          background:
+            "repeat-x bottom center / 100vw auto url('/level_win/top-line.svg')",
+        }}
+      />
+    </div>
+  );
+}
+
+function WinPage1({ nextPage }: { nextPage: () => void }) {
+  useEffect(() => {
+    const tl = createTimeline();
+    tl.add(
+      "#win-content-1",
+      {
+        opacity: [0, 1],
+        duration: 750,
+        ease: "outExpo",
+      },
+      750,
+    )
+      .add(
+        "#win-well-done",
+        {
+          scale: [0, 1],
+          ease: spring({
+            bounce: 0.65,
+            duration: 350,
+          }),
+        },
+        "<-150",
+      )
+      .init();
+  }, []);
+
+  function onContinueButton() {
+    animate("#win-content-1", {
+      scale: [1, 0],
+      duration: 250,
+      ease: "outExpo",
+    }).then(nextPage);
+  }
+
+  return (
+    <div
+      id="win-content-1"
+      className={`fixed inset-0 ${MAX_SCREEN_SIZE} z-50 flex flex-col gap-6 items-center justify-center hidable`}
+    >
+      <img
+        alt="Well Done!"
+        id="win-well-done"
+        src="/level_win/well-done.svg"
+        className="h-[10vh] mt-[-9vh]"
+      />
+      <p className="font-display italic font-bold text-3xl text-white text-center w-[80%]">
+        Remember that one of the most telling marks of a true wanderer is having
+        everything they need within arm's reach. Good luck, and may the gear in
+        your satchel bring you to success! Don't celebrate just yet, though, for
+        there is one more trial you forgot to consider.
+      </p>
+      <button
+        type="button"
+        className="flex font-display text-white text-lg font-bold gap-2 items-center uppercase"
+        onClick={onContinueButton}
+      >
+        <p>CONTINUE</p>
+        <img
+          alt="Continue"
+          src="/levels/back.svg"
+          className="h-[2em] -scale-x-100"
+        />
+      </button>
+    </div>
+  );
+}
+
+function WinPage2({ prevPage }: { prevPage: () => void }) {
+  useEffect(() => {
+    animate("#win-content-2", {
+      scale: [0, 1],
+      duration: 350,
+      ease: "outExpo",
+    });
+  }, []);
+
+  function onBackButton() {
+    animate("#win-content-2", {
+      scale: [1, 0],
+      duration: 250,
+      ease: "outExpo",
+    }).then(prevPage);
+  }
+  return (
+    <div
+      id="win-content-2"
+      className={`fixed inset-0 ${MAX_SCREEN_SIZE} flex items-center justify-evenly z-50`}
+    >
+      <button
+        type="button"
+        onClick={onBackButton}
+        className="flex font-display text-white text-2xl font-bold gap-2 items-center uppercase"
+      >
+        <img alt="Continue" src="/levels/back.svg" className="h-[2.5rem]" />
+        <p>Back</p>
+      </button>
+      <div className="flex flex-col items-center">
+        <img
+          src="/level_win/share-your-results.svg"
+          alt="Share your results!"
+          className="w-[50vw] mb-[-6vh] z-51"
+        />
+        <div className="h-[70vh] w-[30vw] border-14 border-[#361876] bg-[#363636] drop-shadow-[0_15px_34px_rgba(0,0,0,0.25)]" />
+        <button type="button">
+          <img
+            src={"/level_win/download.svg"}
+            alt="Download"
+            className="h-[5vh] mt-10"
+          />
+        </button>
+      </div>
+      <Link
+        to="/"
+        className="flex font-display text-white text-2xl font-bold gap-2 items-center uppercase"
+      >
+        <p>Home</p>
+        <img
+          alt="Continue"
+          src="/levels/back.svg"
+          className="h-[2.5rem] -scale-x-100"
+        />
+      </Link>
+    </div>
+  );
+}
+
+export default function WinScreen() {
+  const root = useRef(null);
+  const pages = [
+    <WinPage1 key={0} nextPage={() => changePage(1)} />,
+    <WinPage2 key={1} prevPage={() => changePage(0)} />,
+  ];
+  const [curPage, setPage] = useState(0);
+
+  function changePage(pageNum: number) {
+    setPage(pageNum);
+  }
+
+  return (
+    <div ref={root} id="win-content">
+      <BgBanner />
+      {pages[curPage]}
+    </div>
+  );
+}
--- app/routes/day1.tsx
+++ app/routes/day1.tsx
@@ -1,16 +1,10 @@
 "use client";
 import { Feedback } from "@dnd-kit/dom";
 import { DragDropProvider, useDraggable, useDroppable } from "@dnd-kit/react";
-import {
-  createScope,
-  createTimeline,
-  cubicBezier,
-  type Scope,
-  spring,
-} from "animejs";
-import { type Ref, useEffect, useRef, useState } from "react";
+import { type Ref, useRef, useState } from "react";
 import { useStopwatch } from "react-timer-hook";
 import { set, values } from "remeda";
+import WinScreen from "~/components/WinScreen";
 import {
   centerOfElement,
   clampToScreenPos,
@@ -26,13 +20,24 @@ const MAX_SCREEN_SIZE =
   "w-screen h-screen max-w-screen min-w-screen max-h-screen min-h-screen";
 
 const CORRECT_ORDER = {
-  laptop: 1,
-  a4: 2,
-  notebook: 3,
-  bluebook: 4,
-  map: 5,
+  laptop: 0,
+  a4: 1,
+  notebook: 2,
+  bluebook: 3,
+  map: 4,
   pencilcase: 6,
   wallet: 7,
+  fan: 8,
+  indexcard: 9,
+  twobytwo: 10,
+  umbrella: 5,
+  waterbottle: 5,
+};
+
+const CORRECT_POSITION = {
+  twobytwo: { x: vwToPx("-8vw"), y: vhToPx("-7vh") },
+  umbrella: { x: 0, y: vwToPx("5vw") },
+  waterbottle: { x: 0, y: vwToPx("-5vw") },
 };
 
 const ITEMS: ItemData[] = [
@@ -41,49 +46,84 @@ const ITEMS: ItemData[] = [
     image: "/day1/Laptop.svg",
     width: "100%",
     initialRotate: "15deg",
-    initialPos: { x: vwToPx("-30vw"), y: vhToPx("20vh"), z: 1 },
+    initialPos: { x: vwToPx("-30vw"), y: vhToPx("20vh"), z: 0 },
   },
   {
     name: "a4",
     image: "/day1/A4.svg",
     width: "90%",
     initialRotate: "15deg",
-    initialPos: { x: vwToPx("20vw"), y: vhToPx("-20vh"), z: 0 },
+    initialPos: { x: vwToPx("20vw"), y: vhToPx("-20vh"), z: 1 },
   },
   {
     name: "notebook",
     image: "/day1/Notebook.svg",
     width: "80%",
     initialRotate: "97deg",
-    initialPos: { x: vwToPx("30vw"), y: vhToPx("35vh"), z: 2 },
+    initialPos: { x: vwToPx("30vw"), y: vhToPx("35vh"), z: 3 },
   },
   {
     name: "bluebook",
     image: "/day1/Bluebook.svg",
     width: "70%",
     initialRotate: "75deg",
-    initialPos: { x: vwToPx("35vw"), y: vhToPx("-10vh"), z: 3 },
+    initialPos: { x: vwToPx("35vw"), y: vhToPx("-10vh"), z: 4 },
   },
   {
     name: "map",
     image: "/day1/Map.svg",
     width: "60%",
     initialRotate: "-6deg",
-    initialPos: { x: vwToPx("-25vw"), y: vhToPx("18vh"), z: 6 },
+    initialPos: { x: vwToPx("-25vw"), y: vhToPx("18vh"), z: 5 },
   },
   {
     name: "pencilcase",
     image: "/day1/Pencil Case.svg",
-    width: "50%",
+    width: "45%",
     initialRotate: "-8deg",
-    initialPos: { x: vwToPx("-25vw"), y: vhToPx("-28vh"), z: 4 },
+    initialPos: { x: vwToPx("-25vw"), y: vhToPx("-28vh"), z: 7 },
   },
   {
     name: "wallet",
     image: "/day1/Wallet.svg",
-    width: "43%",
+    width: "42%",
     initialRotate: "7deg",
-    initialPos: { x: vwToPx("-20vw"), y: vhToPx("-12vh"), z: 4 },
+    initialPos: { x: vwToPx("-20vw"), y: vhToPx("-12vh"), z: 8 },
+  },
+  {
+    name: "fan",
+    image: "/day1/Fan.svg",
+    width: "40%",
+    initialRotate: "80deg",
+    initialPos: { x: vwToPx("-20vw"), y: vhToPx("40vh"), z: 2 },
+  },
+  {
+    name: "indexcard",
+    image: "/day1/IndexCard.svg",
+    width: "38%",
+    initialRotate: "10deg",
+    initialPos: { x: vwToPx("20vw"), y: vhToPx("20vh"), z: 6 },
+  },
+  {
+    name: "twobytwo",
+    image: "/day1/2x2.svg",
+    width: "10%",
+    initialRotate: "-8deg",
+    initialPos: { x: vwToPx("20vw"), y: vhToPx("20vh"), z: 9 },
+  },
+  {
+    name: "umbrella",
+    image: "/day1/Umbrella.svg",
+    width: "50%",
+    initialRotate: "-8deg",
+    initialPos: { x: vwToPx("-20vw"), y: vhToPx("-30vh"), z: 10 },
+  },
+  {
+    name: "waterbottle",
+    image: "/day1/Waterbottle.svg",
+    width: "50%",
+    initialRotate: "70deg",
+    initialPos: { x: vwToPx("30vw"), y: vhToPx("50vh"), z: 11 },
   },
 ];
 
@@ -95,8 +135,17 @@ function defaultItemPositions() {
   return itemPositions;
 }
 
-function withinSnappingPosition(width: number, height: number, pos: Pos) {
-  return Math.abs(pos.x) <= width / 4 && Math.abs(pos.y) <= height / 4;
+const defaultPos: Pos = { x: 0, y: 0, z: 0 };
+function withinSnappingPosition(
+  width: number,
+  height: number,
+  pos: Pos,
+  correctPos: Pos = defaultPos,
+) {
+  return (
+    Math.abs(pos.x - correctPos.x) <= width / 4 &&
+    Math.abs(pos.y - correctPos.y) <= height / 4
+  );
 }
 
 enum LayerDirection {
@@ -138,13 +187,13 @@ function DraggableItem({
         width: itemData.width,
         transformStyle: "preserve-3d",
         rotate: withinSnappingPosition() ? "0deg" : itemData.initialRotate,
-        translate: withinSnappingPosition()
-          ? "0px 0px"
-          : `${clampedPosition.x}px ${clampedPosition.y}px`,
+        translate: `${clampedPosition.x}px ${clampedPosition.y}px`,
         filter: `${isFocusedItem ? "drop-shadow(0 0 16px #00bfff)" : ""}`,
         zIndex: clampedPosition.z,
       }}
-      onClick={enableFocus}
+      onClick={() => {
+        if (withinSnappingPosition()) enableFocus();
+      }}
       onKeyDown={(event) => {
         if (event.key === "Enter") enableFocus();
       }} // a11y compliance
@@ -157,176 +206,6 @@ function DraggableItem({
 function getItemNameFromID(itemName: string) {
   return itemName.substring("day1-".length);
 }
-function BgBanner() {
-  const GRID_OVERLAY_CHILD = "row-start-1 row-span-1 col-start-1 col-span-1";
-  const root = useRef(null);
-  const scope = useRef<Scope | null>(null);
-
-  useEffect(() => {
-    if (!root) {
-      return;
-    }
-    scope.current = createScope({ root }).add((_self) => {
-      const BEZIER_OUT = cubicBezier(0.1, 0.7, 0.5, 1);
-      const tl = createTimeline();
-      tl.label("start")
-        .add("#win-screen", {
-          opacity: [0, 1],
-          duration: 300,
-          ease: "inOut",
-        })
-        .add("#win-white-banner", {
-          x: ["100vw", 0],
-          duration: 300,
-          ease: BEZIER_OUT,
-        })
-        .add("#win-purple-banner", {
-          x: ["-100vw", 0],
-          duration: 300,
-          ease: BEZIER_OUT,
-        })
-        .label("line-entrance")
-        .add(
-          "#win-top-line",
-          {
-            x: ["200vw", "100vw"],
-            duration: 1000,
-            ease: BEZIER_OUT,
-          },
-          "line-entrance",
-        )
-        .add(
-          "#win-bottom-line",
-          {
-            x: ["-200vw", "-100vw"],
-            duration: 1000,
-            ease: BEZIER_OUT,
-          },
-          "line-entrance",
-        )
-        .label("line-scroll")
-        .add(
-          "#win-top-line",
-          {
-            x: ["100vw", 0],
-            duration: 200000,
-            loop: true,
-          },
-          "line-scroll",
-        )
-        .add(
-          "#win-bottom-line",
-          {
-            x: ["-100vw", 0],
-            duration: 200000,
-            loop: true,
-          },
-          "line-scroll",
-        )
-        .add(
-          "#win-well-done",
-          {
-            scale: [0, 1],
-            ease: spring({
-              bounce: 0.65,
-              duration: 300,
-            }),
-          },
-          "line-scroll",
-        )
-        .add(
-          "#win-content-1",
-          {
-            opacity: [0, 1],
-            ease: "inOut",
-            duration: 500,
-          },
-          "<<",
-        );
-
-      tl.init();
-
-      return () => scope.current?.revert();
-    });
-  }, []);
-
-  return (
-    <div
-      ref={root}
-      id="win-screen"
-      className={`fixed inset-0 ${MAX_SCREEN_SIZE} z-50 grid grid-cols-1 grid-rows-1 place-items-center`}
-      style={{
-        background:
-          "radial-gradient(rgba(255,255,255,0.3), rgba(255,255,255,0)), rgba(103,74, 179, 0.6)",
-      }}
-    >
-      <div
-        id="win-white-banner"
-        className={`${GRID_OVERLAY_CHILD} h-[40%] w-full bg-white drop-shadow-[0_4px_21px_37px_rgba(0,0,0,0.1)]`}
-      />
-      <div
-        id="win-purple-banner"
-        className={`${GRID_OVERLAY_CHILD} h-[34%] w-full`}
-        style={{
-          background: "radial-gradient(#5d3e9e, #390f8f), purple",
-          filter: "brightness(0.9)",
-        }}
-      />
-      <div
-        id="win-top-line"
-        className={`${GRID_OVERLAY_CHILD} h-[30%] w-[300vw]`}
-        style={{
-          background:
-            "repeat-x top center / 20% auto url('/level_win/top-line.svg')",
-        }}
-      />
-      <div
-        id="win-bottom-line"
-        className={`${GRID_OVERLAY_CHILD} h-[30%] w-[300vw]`}
-        style={{
-          background:
-            "repeat-x bottom center / 20% auto url('/level_win/top-line.svg')",
-        }}
-      />
-      <img
-        id="win-well-done"
-        src="/level_win/well-done.svg"
-        alt="Well done!"
-        className={`${GRID_OVERLAY_CHILD} h-[11%] relative bottom-[17vh] z-50`}
-      />
-    </div>
-  );
-}
-
-function WinScreen() {
-  return (
-    <>
-      <BgBanner />
-      <div
-        id="#win-content-1"
-        className={`fixed inset-0 ${MAX_SCREEN_SIZE} z-50 flex flex-col gap-6 items-center justify-center`}
-      >
-        <p className="font-display italic font-bold text-3xl text-white text-center w-[80%]">
-          Remember that one of the most telling marks of a true wanderer is
-          having everything they need within arm's reach. Good luck, and may the
-          gear in your satchel bring you to success! Don't celebrate just yet,
-          though, for there is one more trial you forgot to consider.
-        </p>
-        <button
-          type="button"
-          className="flex font-display text-white text-lg font-bold gap-2 items-center uppercase"
-        >
-          <p>CONTINUE</p>
-          <img
-            alt="Continue"
-            src="/levels/back.svg"
-            className="h-[2em] -scale-x-100"
-          />
-        </button>
-      </div>
-    </>
-  );
-}
 
 export default function Level1() {
   const [itemPositions, setItemPositions] = useState(defaultItemPositions());
@@ -337,13 +216,18 @@ export default function Level1() {
   const { hours, minutes, seconds } = useStopwatch({ autoStart: true });
 
   function snapItem(item: string) {
-    if (
-      itemPositions[item].z ===
-      CORRECT_ORDER[item as keyof typeof CORRECT_ORDER]
-    ) {
-      setItemWinState(set(itemWinState, item, true));
+    setItemPosition(item, getSnapPosition(item));
+  }
+
+  function getSnapPosition(item: string) {
+    const correctZ = CORRECT_ORDER[item as keyof typeof CORRECT_ORDER];
+    if (item in CORRECT_POSITION) {
+      return {
+        ...CORRECT_POSITION[item as keyof typeof CORRECT_POSITION],
+        z: correctZ,
+      };
     }
-    setItemPosition(item, { x: 0, y: 0, z: itemPositions[item].z });
+    return { ...defaultPos, z: correctZ };
   }
 
   function printTimer() {
@@ -353,10 +237,27 @@ export default function Level1() {
     return `${minutes}:${seconds}`;
   }
 
-  function setItemPosition(itemName: string, itemPos: Pos) {
-    if (itemName in itemPositions) {
-      setItemPositions(set(itemPositions, itemName, itemPos));
+  function isWinnablePosition(itemName: string, itemPos: Pos) {
+    if (!(itemName in CORRECT_ORDER)) return false;
+    const correctOrder = CORRECT_ORDER[itemName as keyof typeof CORRECT_ORDER];
+    if (itemName in CORRECT_POSITION) {
+      const correctPos =
+        CORRECT_POSITION[itemName as keyof typeof CORRECT_POSITION];
+      return (
+        correctPos.x === itemPos.x &&
+        correctPos.y === itemPos.y &&
+        itemPos.z === correctOrder
+      );
+    } else {
+      return itemPos.x === 0 && itemPos.y === 0 && itemPos.z === correctOrder;
     }
+  }
+
+  function setItemPosition(itemName: string, itemPos: Pos) {
+    setItemPositions(set(itemPositions, itemName, itemPos));
+    setItemWinState(
+      set(itemWinState, itemName, isWinnablePosition(itemName, itemPos)),
+    );
   }
 
   function isItemInWinnableState(itemPos: Pos, itemEl: HTMLElement | null) {
@@ -376,18 +277,33 @@ export default function Level1() {
   }
 
   function switchLayers(dir: LayerDirection) {
-    if (!(focusedItem in itemPositions)) {
+    // allow only if item is in snappable position
+    const snapPos = getSnapPosition(focusedItem);
+    if (
+      !(focusedItem in itemPositions) ||
+      !(
+        itemPositions[focusedItem].x === snapPos.x &&
+        itemPositions[focusedItem].y === snapPos.y
+      )
+    ) {
       return;
     }
 
     const newZ =
       dir === LayerDirection.DOWN
         ? Math.max(itemPositions[focusedItem].z - 1, 0)
-        : Math.min(itemPositions[focusedItem].z + 1, ITEMS.length);
+        : Math.min(itemPositions[focusedItem].z + 1, ITEMS.length - 1);
 
-    if (newZ === CORRECT_ORDER[focusedItem as keyof typeof CORRECT_ORDER]) {
-      setItemWinState(set(itemWinState, focusedItem, true));
-      setFocusedItem("");
+    const itemWithSameZ = Object.keys(itemPositions).find(
+      (item) => itemPositions[item].z === newZ,
+    );
+
+    if (itemWithSameZ) {
+      // swap layers with any item that is on same layer
+      setItemPosition(itemWithSameZ, {
+        ...itemPositions[itemWithSameZ],
+        z: itemPositions[focusedItem].z,
+      });
     }
     setItemPosition(focusedItem, {
       ...itemPositions[focusedItem],
@@ -437,9 +353,11 @@ export default function Level1() {
             const dragItem = getItemNameFromID(source.id as string);
 
             for (const item in itemPositions) {
+              const snapPos = getSnapPosition(item);
               // skip solved items & held object
               if (
-                (itemPositions[item].x === 0 && itemPositions[item].y === 0) ||
+                (itemPositions[item].x === snapPos.x &&
+                  itemPositions[item].y === snapPos.y) ||
                 item === dragItem
               ) {
                 continue;
@@ -488,7 +406,6 @@ export default function Level1() {
               snapItem(dragItem);
             } else {
               setItemPosition(dragItem, newPosition); // update current position
-              setFocusedItem(dragItem); // set focused item to last dragged
             }
           }}
           plugins={(defaults) => [
@@ -503,7 +420,7 @@ export default function Level1() {
                 itemData={item}
                 itemPos={itemPositions[item.name]}
                 isFocusedItem={item.name === focusedItem}
-                enableFocus={() => setFocusedItem(item.name)}
+                enableFocus={() => setFocusedItem("")}
                 withinSnappingPosition={() => {
                   const el = document.getElementById(`day1-${item.name}`);
                   const width = el?.offsetWidth || 0;
@@ -512,6 +429,7 @@ export default function Level1() {
                     width,
                     height,
                     itemPositions[item.name],
+                    getSnapPosition(item.name),
                   );
                 }}
               />
@@ -519,7 +437,7 @@ export default function Level1() {
           })}
         </DragDropProvider>
       </main>
-      <WinScreen />
+      {gameWon() && <WinScreen />}
     </>
   );
 }
