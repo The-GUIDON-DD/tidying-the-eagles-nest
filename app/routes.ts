81d957e08a618db2ad041674c13d051ad6015916
Cheska Huang <julyannahuang@gmail.com>
2026-08-17 13:31:53 +0800

    unlock level 3

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import { index, type RouteConfig, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("/day/1", "routes/day1.tsx"),
-  // route("/day/2", "routes/day2.tsx"),
+  route("/day/2", "routes/day2.tsx"),
   route("/day/3", "routes/day3.tsx"),
   route("/levels", "routes/levels.tsx"),
   route("/credits", "routes/credits.tsx"),
